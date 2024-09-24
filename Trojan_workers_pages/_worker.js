35fd011b80b266ba76f88818d1fb51be971fb69c
freeluyishisan <49578773+freeluyishisan@users.noreply.github.com>
2024-09-24 10:14:31 +0800

    Update _worker.js

--- Trojan_workers_pages/_worker.js
+++ Trojan_workers_pages/_worker.js
@@ -4,7 +4,7 @@ import { connect } from "cloudflare:sockets";
 let Pswd = "trojan";
 const proxyIPs = ["proxy.xxxxxxxx.tk"]; //ts.hpc.tw edgetunnel.anycast.eu.org bestproxy.onecf.eu.org cdn-all.xn--b6gac.eu.org cdn.xn--b6gac.eu.org proxy.xxxxxxxx.tk
 let cn_hostnames = [''];
-let CDNIP = 'www.visa.com.sg'
+let CDNIP = 'www.visa.com'
 // http_ip
 let IP1 = 'www.visa.com'
 let IP2 = 'cis.visa.com'
